d9e0f459789299788b2f1a1299cfeee8b820d555
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-04-25 15:53:46 -0400

    Inicio Condicionais

---
+++ condicoes.sql
@@ -0,0 +1,13 @@
+-- Igualdade (=): Verifica se dois valores são iguais.
+-- Diferente (<> ou !=): Verifica se dois valores são diferentes.
+-- Maior (>): Verifica se um valor é maior que outro.
+-- Maior ou igual (>=): Verifica se um valor é maior ou igual a outro.
+-- Menor (<): Verifica se um valor é menor que outro.
+-- Menor ou igual (<=): Verifica se um valor é menor ou igual a outro.
+-- Está (IN): Verifica se um valor está em um conjunto de valores.
+-- Não está (NOT IN): Verifica se um valor não está em um conjunto de valores.
+
+-- Contenha (LIKE): Verifica se um valor contém um padrão específico.
+
+-- %: Representa zero, um ou vários caracteres no LIKE.
+-- _: Representa exatamente um caractere no LIKE.
