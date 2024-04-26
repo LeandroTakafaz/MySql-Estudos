94323cf494ad70d71cc3feb491b7ed7efa40ab2b
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-04-26 14:40:37 -0400

    Condições do Where

--- condicoes.sql
+++ condicoes.sql
@@ -59,3 +59,16 @@ select * from funcionario where nome like '%A%';  -- Traz todos os Resultados qu
 select * from funcionario where nome like '_O%';  -- O Underline serve para pular a quantidade de caracteres inicias necessário para fazer a consulta.
 
 select * from funcionario where nome not like '__O%'; -- O not like nesse caso seleciona o que não tem essa condicional.
+
+
+------------------------------------------------------------------------
+
+-- Condições do Where
+
+select * from funcionario;
+
+select * from funcionario where cargo = 'Professor' and salario = 3000.52; 		-- Usando o AND você pode pesquisar utilizando dois ou mais tipos de busca, caso corresponda, voltará o resultado
+
+select * from funcionario where cargo = 'Coordenador' or salario = 3000.52; 	-- Usando o OR você pesquisa mais de um tipo de busca e retorna os resultados caso cumpra com pelo menos um requisito.
+
+select * from funcionario where (salario < 3000 or cargo = 'recepcionista') or (cargo = 'Coordenador' or salario = 3000.52);	-- coloque duas buscas entre parenteses e separadas pelo or para ter mais de um tipo de busca.
