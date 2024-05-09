a0481b21c682a82dad596276c535488bd9576c27
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-05-09 11:56:14 -0400

    Expansão Agrupamentos

--- Exercicio 4.sql
+++ Exercicio 4.sql
@@ -13,3 +13,9 @@ select count(name) from city where population > 100000;
 -- Query the average population of all cities in CITY where District is California.
 
 select avg(population) from city where district = 'California';
+
+----------------------------------------------------------------------
+
+-- Query the average population for all cities in CITY, rounded down to the nearest integer. 
+
+select floor(avg(population)) from city;
