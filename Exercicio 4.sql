3794a9a57a961ec3889943a8e601f97b712d3f1f
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-05-09 13:44:04 -0400

    Expansão Agrupamento

--- Exercicio 4.sql
+++ Exercicio 4.sql
@@ -19,3 +19,15 @@ select avg(population) from city where district = 'California';
 -- Query the average population for all cities in CITY, rounded down to the nearest integer. 
 
 select floor(avg(population)) from city;
+
+----------------------------------------------------------------------
+
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+select sum(population) from CITY where countrycode = 'JPN';
+
+----------------------------------------------------------------------
+
+-- Query the difference between the maximum and minimum populations in CITY.
+
+select max(population) - min(population) from city;
