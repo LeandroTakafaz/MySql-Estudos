930a4939eff14ea216baaaa9f5663e48dc3d7eab
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-05-16 09:15:20 -0400

    Padronização - Exercício Bônus

--- Exercicio 7.sql
+++ Exercicio 7.sql
@@ -1,23 +1,23 @@
 -- Exercício Bônus
 
--- 1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.> encolsed in parentheses).
+-- 1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.> enclosed in parentheses).
 -- For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
 
 -- 2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
 -- 'There are a total of [occupation_count] [occupation]s.
 
-SELECT result FROM (
-  (SELECT CONCAT(name, '(', LEFT(occupation, 1), ')') AS result
-   FROM occupations
-   ORDER BY name ASC)
+select result from (
+  (select concat(name, '(', LEFT(occupation, 1), ')') AS result
+   from occupations
+   order by name asc)
    
-  UNION ALL
+  union all
    
-  (SELECT CONCAT('There are a total of ', COUNT(*), ' ', lower(occupation), 's.')
-   FROM occupations
-   GROUP BY occupation)
+  (select concat('There are a total of ', count(*), ' ', lower(occupation), 's.')
+   from occupations
+   group by occupation)
 ) AS combined_results
-ORDER BY CASE 
-  WHEN result LIKE 'There are a total of%' THEN 1
-  ELSE 0
-END, result;
+order by case 
+  when result like 'There are a total of%' then 1
+  else 0
+end, result;
