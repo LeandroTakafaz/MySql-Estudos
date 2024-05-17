59cea2d1692ee3caf63b08f31aa2ee67cd26bfdb
Leandro Takafaz <leandrotakafaz2002@gmail.com>
2024-05-17 14:55:22 -0400

    Padronização - Exercício Avançado

--- Exercicio 7.sql
+++ Exercicio 7.sql
@@ -26,19 +26,19 @@ end, result;
 -- The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 -- Note: Print NULL when there are no more names corresponding to an occupation.
 
-SET @r1 = 0, @r2 = 0, @r3 = 0, @r4 = 0;
-SELECT MIN(Doctor), MIN(Professor), MIN(Singer), MIN(Actor)
-FROM (
-  SELECT CASE
-           WHEN Occupation = 'Doctor' THEN (@r1:=@r1+1)
-           WHEN Occupation = 'Professor' THEN (@r2:=@r2+1)
-           WHEN Occupation = 'Singer' THEN (@r3:=@r3+1)
-           WHEN Occupation = 'Actor' THEN (@r4:=@r4+1) END AS RowNumber,
-         CASE WHEN Occupation = 'Doctor' THEN Name END AS Doctor,
-         CASE WHEN Occupation = 'Professor' THEN Name END AS Professor,
-         CASE WHEN Occupation = 'Singer' THEN Name END AS Singer,
-         CASE WHEN Occupation = 'Actor' THEN Name END AS Actor
-  FROM OCCUPATIONS
-  ORDER BY Doctor, Professor, Singer, Actor
-) Temp
-GROUP BY RowNumber
+set @r1 = 0, @r2 = 0, @r3 = 0, @r4 = 0;
+select MIN(Doctor), MIN(Professor), MIN(Singer), MIN(Actor)
+from (
+  select case
+           when Occupation = 'Doctor' then (@r1:=@r1+1)
+           when Occupation = 'Professor' then (@r2:=@r2+1)
+           when Occupation = 'Singer' then (@r3:=@r3+1)
+           when Occupation = 'Actor' then (@r4:=@r4+1) END AS RowNumber,
+         case when Occupation = 'Doctor' then name end as Doctor,
+         case when Occupation = 'Professor' then name end as Professor,
+         case when Occupation = 'Singer' then name end as Singer,
+         case when Occupation = 'Actor' then name end as Actor
+  from OCCUPATIONS
+  order by Doctor, Professor, Singer, Actor
+) temp
+group by RowNumber;
